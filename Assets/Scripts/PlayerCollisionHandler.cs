93c4a83541c701caf2cdafd1bc9b904211eb21fd
Tijn <tijn.stuifbergen@hva.nl>
2024-09-05 11:54:34 +0200

    started working on camera lockout, and added check for ground collision

---
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    [SerializeField] private Rigidbody player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision trigger)
+    {
+        if (trigger.gameObject.CompareTag("BirdKiller"))
+        {
+            player.velocity = new Vector3(0, 0, -100);
+        }
+    }
+}
